78b2797bd73c340c7a29ca9fc80cc946d7dec827
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-12-09 16:17:57 -0400

    a

--- baoba-ervas-ai/api.php
+++ baoba-ervas-ai/api.php
@@ -1,10 +1,44 @@
 <?php
 
-function responderPergunta($pergunta)
-{
-    $resposta = "Desculpe, não entendi a pergunta.";
+// function responderPergunta(string $pergunta): string
+// {
+//     $resposta = "Desculpe, não entendi a pergunta.";
 
-    if (stripos($pergunta, 'horário') !== false || stripos($pergunta, 'horario') !== false || stripos($pergunta, 'horarios') !== false || stripos($pergunta, 'horarios') !== false) {
+//     if (stripos($pergunta, 'horário') !== false || stripos($pergunta, 'horario') !== false || stripos($pergunta, 'horarios') !== false || stripos($pergunta, 'horarios') !== false) {
+//         $resposta = "Estamos abertos de segunda a sexta, das 8h às 18h, e aos sábados, das 8h às 14h.";
+//     } elseif (stripos($pergunta, 'produtos') !== false || stripos($pergunta, 'produto') !== false) {
+//         $resposta = "Oferecemos uma variedade de produtos naturais, incluindo suplementos, alimentos orgânicos e produtos de cuidados pessoais.";
+//     } elseif (stripos($pergunta, 'pedido') !== false) {
+//         $resposta = "Sim, aceitamos pedidos online através do nosso site. Você pode fazer seu pedido e escolher a opção de entrega.";
+//     } elseif (stripos($pergunta, 'localização') !== false || stripos($pergunta, 'localizacao') !== false) {
+//         $resposta = "Estamos localizados na Avenida Couto Magalhães, 1776, Cidade Várzea Grande | \nRua Feliciano Galdino, 585, Cidade Cuiabá | \nRua Joaquin Murtinho, 319, Cidade Cuiabá | \n. Venha nos visitar!";
+//     }
+
+//     return $resposta;
+// }
+
+function buscarInformacoesWikipedia(string $termo): string
+{
+    $url = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&titles=" . urlencode($termo);
+
+    // Realiza a solicitação à API da Wikipedia
+    $respostaJson = file_get_contents($url);
+
+    // Decodifica a resposta JSON
+    $respostaArray = json_decode($respostaJson, true);
+
+    // Obtém o conteúdo do artigo (se disponível)
+    $pagina = current($respostaArray['query']['pages']);
+    $conteudo = isset($pagina['extract']) ? $pagina['extract'] : "Nenhuma informação encontrada.";
+
+    return $conteudo;
+}
+
+function responderPergunta(string $pergunta): string
+{
+    $resposta = "Desculpe, não encontrei informações para essa pergunta.";
+
+    if (stripos($pergunta, 'horário') !== false || stripos($pergunta, 'horario') !== false || stripos($pergunta, 'horários') !== false) {
         $resposta = "Estamos abertos de segunda a sexta, das 8h às 18h, e aos sábados, das 8h às 14h.";
     } elseif (stripos($pergunta, 'produtos') !== false || stripos($pergunta, 'produto') !== false) {
         $resposta = "Oferecemos uma variedade de produtos naturais, incluindo suplementos, alimentos orgânicos e produtos de cuidados pessoais.";
@@ -12,6 +46,14 @@ function responderPergunta($pergunta)
         $resposta = "Sim, aceitamos pedidos online através do nosso site. Você pode fazer seu pedido e escolher a opção de entrega.";
     } elseif (stripos($pergunta, 'localização') !== false || stripos($pergunta, 'localizacao') !== false) {
         $resposta = "Estamos localizados na Avenida Couto Magalhães, 1776, Cidade Várzea Grande | \nRua Feliciano Galdino, 585, Cidade Cuiabá | \nRua Joaquin Murtinho, 319, Cidade Cuiabá | \n. Venha nos visitar!";
+    } else {
+        // Se a pergunta não se encaixa nas categorias anteriores, busca na Wikipedia
+        $informacoesWikipedia = buscarInformacoesWikipedia($pergunta);
+
+        // Se houver informações na Wikipedia, substitui a resposta padrão
+        if ($informacoesWikipedia !== "Nenhuma informação encontrada.") {
+            $resposta = $informacoesWikipedia;
+        }
     }
 
     return $resposta;
